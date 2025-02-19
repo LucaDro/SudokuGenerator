20b1b8997b2cbe5f3eff8fa8503fef20c59ac81a
Luca <luca.drouillet@gmail.com>
2025-02-19 22:47:11 +0100

    right concept, don't think the concept works

--- sudokuGenerator/Generator.java
+++ sudokuGenerator/Generator.java
@@ -6,7 +6,8 @@ public class Generator {
 
     public void main(String[] args) {
         Integer[][] sudoku = createEmptySudoku();
-        fillDiagonalSquares(sudoku);
+        fillDiagonalBoxes(sudoku);
+        fillGrid(sudoku);
         printSudoku(sudoku);
     }
 
@@ -29,19 +30,68 @@ public class Generator {
         }
     }
 
-    private Integer[][] fillSquare(Integer[][] sudoku, int start, int end) {
+    private Integer[][] fillBox(Integer[][] sudoku, int start, int end) {
         for(int hor=start; hor<end; hor++){
             for(int ver=start; ver<end; ver++){
-                sudoku[hor][ver] = random.nextInt(8)+1;
+                fillSquare(sudoku, hor, ver);
             }
         } 
         return sudoku;
     }
 
-    private Integer[][] fillDiagonalSquares(Integer[][] sudoku){
-        fillSquare(sudoku, 0, 3);
-        fillSquare(sudoku, 3, 6);
-        fillSquare(sudoku, 6, 9);
+    private Integer[][] fillDiagonalBoxes(Integer[][] sudoku){
+        fillBox(sudoku, 0, 3);
+        fillBox(sudoku, 3, 6);
+        fillBox(sudoku, 6, 9);
         return sudoku;
+    }
+
+    private Integer[][] fillGrid(Integer[][] sudoku){
+        for(int hor=0; hor<9; hor++){
+            for(int ver=0; ver<9; ver++){
+                if(sudoku[hor][ver] == 0){
+                    fillSquare(sudoku, hor, ver);
+                }
+            }
+        }
+        return sudoku;
+    }
+
+    private Integer[][] fillSquare(Integer[][] sudoku, int horizontal, int vertical){
+        boolean found = false;
+        OUTERLOOP:
+        while(!found) {
+            int number = random.nextInt(9) + 1;
+            for(int hor=0; hor<9; hor++){
+                if(number==sudoku[hor][vertical]){
+                    continue OUTERLOOP;
+                }
+            }
+            for(int ver=0; ver<9; ver++){
+                if(number==sudoku[horizontal][ver]){
+                    continue OUTERLOOP;
+                }
+            }
+            if(checkBox(sudoku, horizontal, vertical, number)){
+                continue OUTERLOOP;
+            }
+            found = true;
+            sudoku[horizontal][vertical] = number;
+            printSudoku(sudoku);
+        }
+        return sudoku;
+    }
+
+    private boolean checkBox(Integer[][] sudoku, int x, int y, int number){
+        int xStart = (x/3)*3;
+        int yStart = (y/3)*3;
+        for(int hor=xStart; hor<xStart+3; hor++){
+            for(int ver=yStart; ver<yStart+3; ver++){
+                if(sudoku[hor][ver] == number){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
