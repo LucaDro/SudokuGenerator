e7f7bf6e4de567974e1faa4b447ea42d2546f2d8
Luca <luca.drouillet@gmail.com>
2025-02-19 20:38:43 +0100

    main file

---
+++ Main.java
@@ -0,0 +1,8 @@
+import sudokuGenerator.Generator;
+
+public class Main {
+    public static void main(String[] args) {
+        Generator generator = new Generator();
+        generator.main(args);
+    }
+}
