f32a0116ff9f70972b3ad0c76d3368575e736748
Luca <luca.drouillet@gmail.com>
2025-02-25 21:26:13 +0100

    taking out comments

--- sudokuGenerator/Generator.java
+++ sudokuGenerator/Generator.java
@@ -30,58 +30,6 @@ public class Generator {
         }
     }
 
-    // private Integer[][] fillBox(Integer[][] sudoku, int start, int end) {
-    //     for(int hor=start; hor<end; hor++){
-    //         for(int ver=start; ver<end; ver++){
-    //             fillSquare(sudoku, hor, ver);
-    //         }
-    //     } 
-    //     return sudoku;
-    // }
-
-    // private Integer[][] fillDiagonalBoxes(Integer[][] sudoku){
-    //     fillBox(sudoku, 0, 3);
-    //     fillBox(sudoku, 3, 6);
-    //     fillBox(sudoku, 6, 9);
-    //     return sudoku;
-    // }
-
-    // private Integer[][] fillGrid(Integer[][] sudoku){
-    //     for(int hor=0; hor<9; hor++){
-    //         for(int ver=0; ver<9; ver++){
-    //             if(sudoku[hor][ver] == 0){
-    //                 fillSquare(sudoku, hor, ver);
-    //             }
-    //         }
-    //     }
-    //     return sudoku;
-    // }
-
-    // private Integer[][] fillSquare(Integer[][] sudoku, int horizontal, int vertical){
-    //     boolean found = false;
-    //     OUTERLOOP:
-    //     while(!found) {
-    //         int number = random.nextInt(9) + 1;
-    //         for(int hor=0; hor<9; hor++){
-    //             if(number==sudoku[hor][vertical]){
-    //                 continue OUTERLOOP;
-    //             }
-    //         }
-    //         for(int ver=0; ver<9; ver++){
-    //             if(number==sudoku[horizontal][ver]){
-    //                 continue OUTERLOOP;
-    //             }
-    //         }
-    //         if(checkBox(sudoku, horizontal, vertical, number)){
-    //             continue OUTERLOOP;
-    //         }
-    //         found = true;
-    //         sudoku[horizontal][vertical] = number;
-    //         printSudoku(sudoku);
-    //     }
-    //     return sudoku;
-    // }
-
     private Integer[][] fillSquare(Integer[][] sudoku, int x, int y){
         printSudoku(sudoku);
         ArrayList<Integer> tried = new ArrayList<>();
